2dd0bafb4b94e59ff88532b7557bced94ebbee7e
KirillPlatypus <yarovoykirill942@gmail.com>
2022-12-25 01:55:30 +0300

    Init rep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { HostListener } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'BigFrontProject';
+  my ='Моя страница';
+  messege='Сообщения'
+  readonly my_c ='Моя страница';
+  readonly messege_c='Сообщения'
+
+  public isOpened: boolean = true;
+  public isLowerThen: boolean = false;
+  screenWidth: number | undefined;
+  
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() {
+      if(window.innerWidth<720)
+      {
+        this.my = '';
+        this.messege = '';
+      }
+      else
+      {
+        this.my = this.my_c;
+        this.messege = this.messege_c;
+      }
+    }
+  constructor(private iconRegistry: MatIconRegistry, ){}
+
+  ngOnInit()
+  {
+    this.iconRegistry.setDefaultFontSetClass("material-icons-outlined");
+  }
+
+  public openCloseSidenav() :void
+  {
+    this.isOpened = !this.isOpened;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//material
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
